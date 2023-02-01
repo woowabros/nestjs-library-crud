c6a212f97a0b84a497f184e69b1e204d7136c139
dh.kang <dh.kang@woowahan.com>
2023-02-01 17:45:09 +0900

    fix: lint-staged to check type

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     '*.+(ts|js|json)': ['eslint --ext .ts,.js --fix', 'prettier --write'],
+    '*.+(ts)': [() => 'tsc -p tsconfig.lib.json --noEmit', () => 'tsc -p tsconfig.spec.json --noEmit'],
 };

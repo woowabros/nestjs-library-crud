d73bde8e0379a874a9baf99d76e05d2256272c4f
dh.kang <dh.kang@woowahan.com>
2023-02-10 14:00:35 +0900

    test: configure test enviroment for PostgreSQL

--- jest.config.ts
+++ jest.config.ts
@@ -35,6 +35,7 @@ const config: Config = {
         },
     },
     coveragePathIgnorePatterns: ['<rootDir>/jest.config.ts', '.mock.ts'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+import { config } from 'dotenv';
+config({ path: __dirname + '/.env.test' });

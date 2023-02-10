e61d569c6c65c893c3536984a6c7d49c716710c5
jiho.park <jiho@woowahan.com>
2023-02-10 17:20:32 +0900

    feat: define search body swagger interface

--- src/lib/dto/request-search.dto.ts
+++ src/lib/dto/request-search.dto.ts
@@ -1,12 +1,44 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
 import { Sort } from '../interface';
-import { QueryFilter } from '../interface/query-operation.interface';
+import { QueryFilter, operators } from '../interface/query-operation.interface';
 
 export class RequestSearchDto<T> {
+    @ApiPropertyOptional({ description: 'select fields', isArray: true, type: [String] })
     select?: Array<keyof Partial<T>>;
+
+    @ApiPropertyOptional({
+        description: 'where conditions',
+        isArray: true,
+        type: 'object',
+        properties: {
+            operator: {
+                description: 'operator',
+                type: 'string',
+                enum: operators,
+            },
+            operand: {
+                description: 'operand',
+                type: 'any',
+                examples: ['value', 1, true],
+            },
+            not: {
+                description: 'not operator',
+                type: 'boolean',
+                example: true,
+            },
+        },
+    })
     where?: Array<QueryFilter<T>>;
+
+    @ApiPropertyOptional({ description: 'order' })
     order?: {
         [key in keyof Partial<T>]: Sort;
     };
+
+    @ApiPropertyOptional({ description: 'withDeleted' })
     withDeleted?: boolean;
+
+    @ApiPropertyOptional({ description: 'take' })
     take?: number;
 }

b80a2bfba91bd121fca27b27e5d9ea8539a71450
Jongmin Lee <jakelee@woowahan.com>
2023-05-17 13:43:32 +0900

    feat: add interface comment

--- src/lib/interface/decorator-option.interface.ts
+++ src/lib/interface/decorator-option.interface.ts
@@ -32,6 +32,9 @@ export interface PrimaryKey {
     type?: ColumnType;
 }
 
+/**
+ * See `crud.policy.ts` to check default values.
+ */
 export interface CrudOptions {
     /**
      * Entity class which CRUD operations will be performed
@@ -39,7 +42,8 @@ export interface CrudOptions {
     entity: typeof BaseEntity;
 
     /**
-     * enable Debug logging, default is false
+     * enable Debug logging
+     * @default false
      */
     logging?: boolean;
 
@@ -60,49 +64,61 @@ export interface CrudOptions {
             params?: string[];
             /**
              * If set to true, soft-deleted enitity could be included in the result.
+             * @default false
              */
             softDelete?: boolean;
+            /**
+             * @default false
+             */
             relations?: false | string[];
         } & RouteBaseOption;
         [Method.READ_MANY]?: {
             /**
              * Way to order the result
+             * @default Sort.ASC
              */
             sort?: Sort | `${Sort}`;
             /**
              * Type of pagination to use. Currently 'offset' and 'cursor' are supported.
+             * @default PaginationType.CURSOR
              */
             paginationType?: PaginationType | `${PaginationType}`;
             /**
              * Max number of entities should be taken.
+             * @default 100
              */
             numberOfTake?: number;
             /**
              * What relations of entity should be loaded.
              * If set to false or an empty array, no relations will be loaded.
+             * @default false
              */
             relations?: false | string[];
             /**
              * If set to true, soft-deleted enitity could be included in the result.
+             * @default true
              */
             softDelete?: boolean;
         } & Omit<RouteBaseOption, 'response'>;
         [Method.SEARCH]?: {
             /**
-             * Default number of entities should be taken.
+             * Default number of entities should be taken. See `crud.policy.ts` for more details.
+             * @default 20
              */
             numberOfTake?: number;
             /**
-             * Max number of entities should be taken.
+             * Max number of entities should be taken. See `crud.policy.ts` for more details.
+             * @default 100
              */
             limitOfTake?: number;
             /**
-             * What relations of entity should be loaded.
-             * If set to false or an empty array, no relations will be loaded.
+             * What relations of entity should be loaded. If set to false or an empty array, no relations will be loaded. See `crud.policy.ts` for more details.
+             * @default false
              */
             relations?: false | string[];
             /**
-             * If set to true, soft-deleted enitity could be included in the result.
+             * If set to true, soft-deleted enitity could be included in the result. See `crud.policy.ts` for more details.
+             * @default true
              */
             softDelete?: boolean;
         } & RouteBaseOption;
@@ -115,8 +131,8 @@ export interface CrudOptions {
             };
             /**
              * Configures ways to save author information to the Entity after the operation completed.
-             * It updates Entity's author.property field with author.filter's value from express's Request object.
-             * If author.filter is not found in the Request object, author.value will be used as default.
+             * It updates Entity's `author.property` field with respect to the value of `author.filter` from express's Request object.
+             * If `author.filter` is not found in the Request object, author.value will be used as default.
              */
             author?: Author;
         } & RouteBaseOption;
@@ -140,8 +156,8 @@ export interface CrudOptions {
             /**
              * Configures ways to save author information to the Entity after the operation completed.
              *
-             * It updates Entity's author.property field with author.filter's value from express's Request object.
-             * If author.filter is not found in the Request object, author.value will be used as default.
+             * It updates Entity's `author.property` field with respect to the value of `author.filter` from express's Request object.
+             * If `author.filter` is not found in the Request object, author.value will be used as default.
              */
             author?: Author;
         } & RouteBaseOption;
@@ -158,13 +174,14 @@ export interface CrudOptions {
             params?: string[];
             /**
              * If set to true, the enitity will be soft deleted. (Records the delete date of the entity)
+             * @default true
              */
             softDelete?: boolean;
             /**
              * Configures ways to save author information to the Entity after the operation completed.
              *
-             * It updates Entity's author.property field with author.filter's value from express's Request object.
-             * If author.filter is not found in the Request object, author.value will be used as default.
+             * It updates Entity's `author.property` field with respect to the value of `author.filter` from express's Request object.
+             * If `author.filter` is not found in the Request object, author.value will be used as default.
              */
             author?: Author;
         } & RouteBaseOption;
@@ -188,8 +205,8 @@ export interface CrudOptions {
             /**
              * Configures ways to save author information to the Entity after the operation completed.
              *
-             * It updates Entity's author.property field with author.filter's value from express's Request object.
-             * If author.filter is not found in the Request object, author.value will be used as default.
+             * It updates Entity's `author.property` field with respect to the value of `author.filter` from express's Request object.
+             * If `author.filter` is not found in the Request object, author.value will be used as default.
              */
             author?: Author;
         } & RouteBaseOption;
@@ -207,8 +224,8 @@ export interface CrudOptions {
             /**
              * Configures ways to save author information to the Entity after the operation completed.
              *
-             * It updates Entity's author.property field with author.filter's value from express's Request object.
-             * If author.filter is not found in the Request object, author.value will be used as default.
+             * It updates Entity's `author.property` field with respect to the value of `author.filter` from express's Request object.
+             * If `author.filter` is not found in the Request object, author.value will be used as default.
              */
             author?: Author;
         } & RouteBaseOption;

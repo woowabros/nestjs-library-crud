5d8eb49dda5517a1e06e024d7875b0a491af9893
Phong.Do <phong.do@vn.woowahan.com>
2023-06-09 22:37:54 +0700

    fix: fix build script

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
     '*.+(ts|js|json)': ['eslint --ext .ts,.js --fix', 'prettier --write'],
-    '*.+(ts)': [() => 'tsc -p tsconfig.lib.json --noEmit', () => 'tsc -p tsconfig.spec.json --noEmit'],
+    '*.+(ts)': [() => 'tsc -p tsconfig.cjs.json --noEmit', () => 'tsc -p tsconfig.spec.json --noEmit'],
 };

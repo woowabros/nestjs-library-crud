87b7311cda91b28f32ef0f66bf9a2709216d5ee0
Youngho Kim <jaden.kim5179@gmail.com>
2024-05-24 09:50:37 +0900

    feat: add validation to check if CrudService is included as member of controller (#545)

---
+++ src/lib/crud.decorator.spec.ts
@@ -0,0 +1,36 @@
+/* eslint-disable max-classes-per-file */
+import { BaseEntity, Entity, Repository } from 'typeorm';
+
+import { Crud } from './crud.decorator';
+import { CrudService } from './crud.service';
+
+describe('Crud.Decorator', () => {
+    @Entity('test')
+    class TestEntity extends BaseEntity {}
+
+    @Crud({
+        entity: TestEntity,
+    })
+    class TestController {
+        constructor(public readonly crudService: any) {}
+    }
+
+    describe('should check crudService is included as a member of target, and is instance of CrudService', () => {
+        it('crudService(instance of CrudService) not throw error', () => {
+            const mockRepository = {
+                metadata: {
+                    primaryColumns: [],
+                    columns: [],
+                },
+            };
+            const crudService = new CrudService(mockRepository as unknown as Repository<TestEntity>);
+            expect(() => new TestController(crudService)).not.toThrow();
+        });
+
+        test.each([undefined, null, 'wrong', {}, []])('crudService(%p) throw error', (crudService: any) => {
+            expect(() => new TestController(crudService)).toThrow(
+                new TypeError('controller should include member crudService, which is instance of CrudService'),
+            );
+        });
+    });
+});
--- src/lib/crud.decorator.ts
+++ src/lib/crud.decorator.ts
@@ -1,10 +1,20 @@
 import { CrudRouteFactory } from './crud.route.factory';
+import { CrudService } from './crud.service';
 import { CrudOptions } from './interface';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Crud =
     (options: CrudOptions) =>
-    (target: unknown): void => {
+    <T extends { new (...args: any[]): any }>(target: T) => {
         const crudRouteFactory = new CrudRouteFactory(target, options);
         crudRouteFactory.init();
+
+        return class extends target {
+            constructor(...args: any[]) {
+                super(...args);
+                if (!(this.crudService instanceof CrudService)) {
+                    throw new TypeError('controller should include member crudService, which is instance of CrudService');
+                }
+            }
+        };
     };
